756f377a8a6ac72091df8e65a0d0dff762abac44
samyak619 <samyakhadse@gmail.com>
2024-07-17 23:41:53 +0530

    chore: Update Login page styling

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify';
 import { handleSuccess, handleError } from '../../utils';
+import '../Login/Login.css'
 
 function Login() {
 
